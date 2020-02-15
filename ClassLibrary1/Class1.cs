ebe1c3790349f40d500699201c83a12d3de5ff0b
praveen kumar <praveenmjthotta@outlook.com>
2020-02-15 19:52:20 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+		private int myVar;
+
+		public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+	}
+}
